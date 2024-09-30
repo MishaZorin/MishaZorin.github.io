2829f29a78c4b2814a9eba973fc979fbe6af99d7
MishaZorin <165201586+MishaZorin@users.noreply.github.com>
2024-09-30 18:21:02 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,63 +1,60 @@
-let modal = document.getElementById('modal')
-let home = document.getElementById('home')
-let modal_home = document.getElementById('modal_home')
-let modal1 = document.getElementById('modal1')
-let modal2 = document.getElementById('modal2')
-let galery = document.getElementById('galery')
-let galeryDesktop = document.getElementById('galeryDesktop')
-let galery_button = document.getElementById('galery_button')
-let about_usDesktop = document.getElementById('about_usDesktop')
-let contacts = document.getElementById('contacts')
-let about_us = document.getElementById('about_us')
-let contactsDesktop = document.getElementById('contactsDesktop')
-about_usDesktop.onclick = function () {
-    modal.style.display = 'block'
-    modal1.style.display = 'none'
-    modal2.style.display = 'none'
-    modal_home.style.display = 'none'
+
+let services = document.getElementById('services')
+let agent = document.getElementById('agent')
+let click = document.getElementById('click')
+let click1 = document.getElementById('click1')
+let click2 = document.getElementById('click2')
+let click3 = document.getElementById('click3')
+let click_modal = document.getElementById('click_modal')
+let cases_blog = document.getElementById('cases_blog')
+let services_modal_show = false
+let agenstvo_show = false
+let services_modal = document.getElementById('services_modal')
+let agenstvo = document.getElementById('agenstvo')
+services.onclick = function () {
+    services_modal.style.display = 'block'
+    agenstvo.style.display = 'none'
+
+    services_modal_show = !services_modal_show
+    if (services_modal_show) {
+        services_modal.style.display = 'block'
+
+    }
+    else {
+        services_modal.style.display = 'none'
+    }
 
 }
-about_us.onclick = function () {
-    modal.style.display = 'block'
-    modal1.style.display = 'none'
-    modal2.style.display = 'none'
-    modal_home.style.display = 'none'
+agent.onclick = function () {
+    services_modal.style.display = 'none'
+    agenstvo.style.display = 'block'
+    agenstvo_show = !agenstvo_show
+    if (agenstvo_show) {
+        agenstvo.style.display = 'block'
+
+    }
+    else {
+        agenstvo.style.display = 'none'
+    }
+
 
 }
-contactsDesktop.onclick = function () {
-    modal1.style.display = 'block'
-    modal2.style.display = 'none'
-    modal.style.display = 'none'
-    modal_home.style.display = 'none'
+click.onclick = function () {
+    click_modal.style.display = 'block'
 }
-contacts.onclick = function () {
-    modal1.style.display = 'block'
-    modal2.style.display = 'none'
-    modal.style.display = 'none'
-    modal_home.style.display = 'none'
+click1.onclick = function () {
+    click_modal.style.display = 'block'
 }
-
-galery_button.onclick = function () {
-    modal2.style.display = 'block'
-    modal_home.style.display = 'none'
-    modal.style.display = 'none'
-    modal1.style.display = 'none'
+click2.onclick = function () {
+    click_modal.style.display = 'block'
 }
-galeryDesktop.onclick = function () {
-    modal2.style.display = 'block'
-    modal_home.style.display = 'none'
-    modal.style.display = 'none'
-    modal1.style.display = 'none'
+click3.onclick = function () {
+    click_modal.style.display = 'block'
 }
-galery.onclick = function () {
-    modal2.style.display = 'block'
-    modal_home.style.display = 'none'
-    modal.style.display = 'none'
-    modal1.style.display = 'none'
+click_modal.onclick = function () {
+    click_modal.style.display = 'none'
+        
 }
-home.onclick = function () {
-    modal.style.display = 'none'
-    modal1.style.display = 'none'
-    modal2.style.display = 'none'
-    modal_home.style.display = 'block'
+click_modal.children[0].onclick = function (event) {
+    event.stopPropagation()
 }
